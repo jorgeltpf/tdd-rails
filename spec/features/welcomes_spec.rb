6d04850d87230953df58f52025b5d810d800a8ca
Jorge Titoneli <jorgeltpf@gmail.com>
2018-06-04 22:43:13 -0300

    Aula 104 - Testando o Link de Cadastro

--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 feature "Welcome", type: :feature do
-  scenario "Mostra a mensagem de Bem-vindo" do
-    visit('/')
+  scenario 'Mostra a mensagem de Bem-vindo' do
+    visit(root_path)
     expect(page).to have_content('Bem-vindo')
   end
+
+  scenario 'Verificar o link Cadastro de Clientes' do
+    visit(root_path)
+    # expect(page).to have_selector('ul li a')
+    # expect(page).to have_link('Cadastro de Clientes')
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
+  end
+
 end
